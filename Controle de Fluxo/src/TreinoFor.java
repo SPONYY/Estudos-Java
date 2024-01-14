2936bca331b1d2598c8e1ca8ffbd79391466fce8
SPONYY <h.cauadgr@gmail.com>
2024-01-14 15:35:32 -0300

    a

--- Controle de Fluxo/src/TreinoFor.java
+++ Controle de Fluxo/src/TreinoFor.java
@@ -26,65 +26,65 @@ public class TreinoFor {
         }
     }
 
-    public static void paresEImparesContar(){
-        Scanner receberNumeroInteiro = new Scanner(System.in).useLocale(Locale.US);
+        public static void paresEImparesContar(){
+            Scanner receberNumeroInteiro = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Digite um número inteiro = ");
-        int numero = receberNumeroInteiro.nextInt();
-        System.out.println();
-
-        if (numero>=1) {
-            System.out.println("Todos os números impares de 1 até " + numero + " = ");
-            System.out.println();
-            forNumerosImpares(numero);
-            System.out.println();
-
-            System.out.println("Todos os números pares de 2 até " + numero + " = ");
-            System.out.println();
-            forNumerosPares(numero);
-            
-        }else{System.out.println("Informe um número maior ou igual a 1");}
-    }
-
-    public static void forNumerosPares(int numero){
-        for (int numeroInicial = 2 ; numeroInicial <= numero; numeroInicial+=2) {
-            System.out.println(numeroInicial + " ");
-            try {
-                Thread.sleep(200);
-            }catch (InterruptedException e) {
-                e.printStackTrace(); //Imprime informações sobre a exceção no console.
-            }
-        }
-    }
-    
-    public static void forNumerosImpares(int numero){
-        for (int numeroInicial = 1 ; numeroInicial <= numero; numeroInicial+=2) {
-            System.out.println(numeroInicial + " ");
-
-            try {
-                Thread.sleep(200);
-            }catch (InterruptedException e) {
-                e.printStackTrace(); //Imprime informações sobre a exceção no console.
-            }
-        }
-
-    }
-
-    public static void acharOMaiorNumero(){
-        Scanner receberNumeroInteiro = new Scanner(System.in).useLocale(Locale.US);
-        int maior = Integer.MIN_VALUE;
-
-        for (int i = 1; i <= 10; i++) {
-            
-            System.out.println("Digite o " + i + "º número: ");
+            System.out.println("Digite um número inteiro = ");
             int numero = receberNumeroInteiro.nextInt();
+            System.out.println();
 
-            if (numero > maior) {
-                maior = numero;
+            if (numero>=1) {
+                System.out.println("Todos os números impares de 1 até " + numero + " = ");
+                System.out.println();
+                forNumerosImpares(numero);
+                System.out.println();
+
+                System.out.println("Todos os números pares de 2 até " + numero + " = ");
+                System.out.println();
+                forNumerosPares(numero);
+                
+            }else{System.out.println("Informe um número maior ou igual a 1");}
+        }
+
+        public static void forNumerosPares(int numero){
+            for (int numeroInicial = 2 ; numeroInicial <= numero; numeroInicial+=2) {
+                System.out.println(numeroInicial + " ");
+                try {
+                    Thread.sleep(200);
+                }catch (InterruptedException e) {
+                    e.printStackTrace(); //Imprime informações sobre a exceção no console.
+                }
             }
         }
-        System.out.println();
-        System.out.println("O maior número é o: " + maior);
+        
+        public static void forNumerosImpares(int numero){
+            for (int numeroInicial = 1 ; numeroInicial <= numero; numeroInicial+=2) {
+                System.out.println(numeroInicial + " ");
 
-    }
+                try {
+                    Thread.sleep(200);
+                }catch (InterruptedException e) {
+                    e.printStackTrace(); //Imprime informações sobre a exceção no console.
+                }
+            }
+
+        }
+
+        public static void acharOMaiorNumero(){
+            Scanner receberNumeroInteiro = new Scanner(System.in).useLocale(Locale.US);
+            int maior = Integer.MIN_VALUE;
+
+            for (int i = 1; i <= 10; i++) {
+                
+                System.out.println("Digite o " + i + "º número: ");
+                int numero = receberNumeroInteiro.nextInt();
+
+                if (numero > maior) {
+                    maior = numero;
+                }
+            }
+            System.out.println();
+            System.out.println("O maior número é o: " + maior);
+
+        }
 }
